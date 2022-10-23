4e36dc452032b429561ae08170d15d4cbb0954cc
Eric K <kurkowskieric2424@gmail.com>
2022-10-22 20:42:34 -0400

    Added information
    Includes a new temporary project section.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 // Initializes the carousel
 var myCarousel = document.querySelector('#myCarousel')
 var carousel = new bootstrap.Carousel(myCarousel)
@@ -7,3 +8,4 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+*/
